51b8ec70e7ea798fe8c1f8b5bb71cbd068b038ca
Prakash Jangid <prakashjangid7357@gmail.com>
2026-07-29 00:54:24 +0530

    fix: update background color for SignIn and SignUp pages to maintain consistency

--- frontend/src/features/auth/pages/SignIn.jsx
+++ frontend/src/features/auth/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/clerk-react";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen bg-[#50f61d] flex items-center justify-center">
+    <div className="min-h-screen bg-[#0b1120] flex items-center justify-center">
       <SignIn />
     </div>
   );
--- frontend/src/features/auth/pages/SignUp.jsx
+++ frontend/src/features/auth/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/clerk-react";
 
 export default function SignUpPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex bg-[#0b1120] min-h-screen items-center justify-center">
       <SignUp />
     </div>
   );
